6752e9e22de49b85073af573125df459dda7e20c
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2020-08-19 22:18:27 -0300

    Initial commit

---
+++ src/main/java/com/springcourse/SpringCourseApplication.java
@@ -0,0 +1,16 @@
+package com.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseApplication.class, args);
+	}
+
+	int i=0;
+	int x=0;
+
+}
