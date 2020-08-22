996c09c2862ab4c1b7f499a3cb2c17c024f1c9ad
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2020-08-21 22:26:55 -0300

    mapeamento de tabelas
    mapeamento de tabelas

--- src/main/java/com/springcourse/domain/User.java
+++ src/main/java/com/springcourse/domain/User.java
@@ -3,6 +3,15 @@ package com.springcourse.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import com.springcourse.domain.enums.Role;
 
 import lombok.AllArgsConstructor;
@@ -13,14 +22,36 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name = "user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length=75, nullable = false)
 	private String name;
+	
+	@Column(length=75, nullable = false, unique = true)
+	private String email;
+	
+	@Column(length=75, nullable = false)
 	private String password;
+	
 	// relacionamento com class Role (enums)
+	@Column(length=20, nullable = false)
+	@Enumerated(EnumType.STRING)  // notacao para linkar atributo com class enum respectiva
 	private Role role;
+	
+	// um usuario pode ter varios pedidos
+	// relaciona chava estrangeira da classe user com classe request
+	@OneToMany(mappedBy= "user")
 	private List<Request> request = new ArrayList<Request>();
+	
+	// um usuario pode ter varios pedidos
+	// relaciona chava estrangeira da classe user com classe requestStage
+	@OneToMany(mappedBy= "user")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
+	
 	
 	
 
