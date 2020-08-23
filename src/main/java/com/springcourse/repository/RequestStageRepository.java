89961ab09c73550d2842cfa99044ecbab0678b42
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2020-08-22 21:14:33 -0300

    Create RequestRepository
    Create RequestRepository

---
+++ src/main/java/com/springcourse/repository/RequestStageRepository.java
@@ -0,0 +1,24 @@
+package com.springcourse.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.enums.RequestState;
+
+@Repository
+public interface RequestStageRepository extends JpaRepository<RequestStage, Long>{
+
+    //metodo lista de estagio do pedido	
+	public List<RequestStage> findAllByRequestId(Long id);
+	
+	// metodo, para query que deve pegar alteracao do estado do pedido
+	@Query("update Request set state=?2 where id=?1")
+	public Request updateStatus (Long id, RequestState state);
+	
+	
+}
