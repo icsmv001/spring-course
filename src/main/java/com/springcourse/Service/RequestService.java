7a575b6abb0931b47fdafd734a551674b5703ee9
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2020-08-26 22:36:11 -0300

    criado o servico -- RequestService
    criado o servico -- RequestService

---
+++ src/main/java/com/springcourse/Service/RequestService.java
@@ -0,0 +1,61 @@
+package com.springcourse.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.enums.RequestState;
+import com.springcourse.repository.RequestRepository;
+
+@Service
+public class RequestService {
+	@Autowired private RequestRepository requestRepository;
+	
+	//save
+	public Request save(Request request) {
+		// o estado do Novo pedido na abertura deve ser OPEN
+		// a data do Novo pedido e a data corrente
+		request.setState(RequestState.OPEN);
+		request.setCreationDate(new Date());
+		
+		Request createdRequest = requestRepository.save(request);
+		return createdRequest;
+	}
+		
+	// update
+	public Request update(Request request) {
+
+		Request updateRequest = requestRepository.save(request);
+		return updateRequest;
+	}
+	
+	// get
+	public Request getById (long id) {
+		Optional<Request> result = requestRepository.findById(id);
+		return result.get();
+	}
+	
+	
+	//list
+	public List<Request> listAll () {
+		List<Request> requests = requestRepository.findAll();
+		return requests;
+	}
+	
+	
+	
+	
+	
+	
+	//list by owner  -- lista de pedidos por usuario
+	public List<Request> listAllByOwnerId (Long owneId) {
+		List<Request> requests = requestRepository.findAllByOwnerId(owneId);
+		return requests;
+	}
+		
+ 	
+}
