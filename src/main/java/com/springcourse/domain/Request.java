63152e54a2a94cf00911a2b49919a4ffbe3a9985
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2020-10-08 22:08:13 -0300

    ignore stag during request serialization

--- src/main/java/com/springcourse/domain/Request.java
+++ src/main/java/com/springcourse/domain/Request.java
@@ -67,8 +67,6 @@ public class Request implements Serializable{
 	@Getter(onMethod = @__({@JsonIgnore})) // notacao para que no momento da serializacao do request, seja ignorado o relacionamento com o stages, que tem um campo id_request tambem.
 	@OneToMany(mappedBy = "request")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
-
-
-		
+	
 	
 }
