a4886a80af93cbaed639b14b1430caa3556f502e
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2020-12-10 21:12:13 -0300

    Criando class SaveUpdateRoleDto e validando entradas

--- src/main/java/com/springcourse/dto/UserUpdateRoleDto.java
+++ src/main/java/com/springcourse/dto/UserUpdateRoleDto.java
@@ -1,6 +1,7 @@
 package com.springcourse.dto;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.springcourse.domain.enums.Role;
 
@@ -10,9 +11,10 @@ import lombok.Setter;
 
 @Getter @Setter
 public class UserUpdateRoleDto {
-	@NotNull
+	@NotNull ( message = "Role required" )
 	private Role role;
 }
+
 
 
  
