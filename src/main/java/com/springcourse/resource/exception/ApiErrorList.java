b77f7420a9b8cd112a8127d708c1409a8cea68bb
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2020-12-11 22:33:16 -0300

    Refatorando codigo - retornando lista de erros de validacao

---
+++ src/main/java/com/springcourse/resource/exception/ApiErrorList.java
@@ -0,0 +1,28 @@
+package com.springcourse.resource.exception;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
+public class ApiErrorList extends ApiError {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<String> errors;
+	
+	public ApiErrorList(int code, String msg, Date date, List<String> erros) {
+		super(code, msg, date);
+	    this.errors = erros;
+	    
+		
+		
+	}
+
+}
