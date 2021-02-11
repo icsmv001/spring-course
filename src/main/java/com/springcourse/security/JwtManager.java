682cb8c521a959ee2cf86b32a33a862c0dfb8bba
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2021-02-10 23:02:35 -0300

    add parse token metode

--- src/main/java/com/springcourse/security/JwtManager.java
+++ src/main/java/com/springcourse/security/JwtManager.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import com.springcourse.constant.SecurityConstants;
 import com.springcourse.dto.UserLoginResponsedto;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -35,4 +37,17 @@ public class JwtManager {
 		return new UserLoginResponsedto(jwt, expireIn , SecurityConstants.JWT_PROVIDER) ;
 	}
 
+	// metodo que valida o token, faz o parse
+	public Claims parseTokens(String jwt)  throws JwtException {
+		Claims claims = Jwts.parser()
+				.setSigningKey(SecurityConstants.API_KEY.getBytes())
+				.parseClaimsJwt(jwt)
+				.getBody();
+		
+				return claims;
+	}
+		
+			
+	
+	
 }
