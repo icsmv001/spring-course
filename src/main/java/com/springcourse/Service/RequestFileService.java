965ad219337e7047096429bf78c8318ba5d930ac
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2021-02-15 17:40:20 -0300

    integrate request file server with s3 service

--- src/main/java/com/springcourse/Service/RequestFileService.java
+++ src/main/java/com/springcourse/Service/RequestFileService.java
@@ -1,24 +1,61 @@
 package com.springcourse.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.springcourse.Service.s3.S3Service;
+import com.springcourse.domain.Request;
 import com.springcourse.domain.RequestFile;
-import com.springcourse.domain.RequestStage;
 import com.springcourse.model.PageModel;
 import com.springcourse.model.PageRequestModel;
+import com.springcourse.model.UploadedFileModel;
 import com.springcourse.repository.RequestFileRepository;
 
 @Service
-public class RequestFilesService {
+public class RequestFileService {
 	
 	@Autowired
 	private RequestFileRepository fileRepository;
-	//upload 
 	
+	
+	@Autowired
+	private S3Service s3Service;
+	
+	
+	//upload 
+	public List<RequestFile> upload(Long requestId, MultipartFile[] files){
+		
+		List<UploadedFileModel> uploadedFiles =  s3Service.upload(files);
+		
+		List<RequestFile> requestFiles = new ArrayList<RequestFile>();
+		
+		uploadedFiles.forEach(uploadedFile -> {
+			RequestFile file = new RequestFile();
+			file.setName(uploadedFile.getName());
+			file.setLocation(uploadedFile.getLocation());
+			
+			//montar um pedido de requisicao
+			Request request = new Request();
+			request.setId(requestId);
+			
+			file.setRequest(request);
+			
+			requestFiles.add(file);
+			
+			
+		});
+		
+		return fileRepository.saveAll(requestFiles);
+		
+		 
+	}
 	
 	
 	// list 
