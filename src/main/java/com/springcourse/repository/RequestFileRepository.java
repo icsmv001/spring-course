0c4e499b1386f7e63be88aad03f89cfb52ab9c73
icsmv001 <46507227+icsmv001@users.noreply.github.com>
2021-02-15 12:58:05 -0300

    create request file repository

--- src/main/java/com/springcourse/repository/RequestFileRepository.java
+++ src/main/java/com/springcourse/repository/RequestFileRepository.java
@@ -1,11 +1,22 @@
 package com.springcourse.repository;
 
-import org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.RequestFile;
 
  
 
 @Repository
-public class RequestFileRepository extends JpaRepositoryConfigExtension <RequestFile, Long>{
+public class RequestFileRepository extends  JpaRepository <RequestFile, Long>{
+	
+	// metodo que faz upload
+    public Page<RequestFile> findAllByrequestId(Long id, Pageable pageable);
+	
+	//metodo para listar os ficheiros de um pedido
+	
 
 }
